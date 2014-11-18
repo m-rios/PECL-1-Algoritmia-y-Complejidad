84332330e6d96295d57c131b72593d3d0179a63b
Mario Rios <mrm994@gmail.com>
2014-11-18 19:17:33 +0100

    Initial Commit

---
+++ PECL1AyC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PECL1AyC
+//
+//  Created by Mario on 18/11/14.
+//  Copyright (c) 2014 Mario. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
